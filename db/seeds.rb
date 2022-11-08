fa793321187bea86349df8c6c98ab0964f6dbd78
Hafid Qarchi <haqarchi@gmail.com>
2022-11-08 14:29:04 +0900

    seeded the db with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'start creating'
+10.times do
+  Article.create!(title: Faker::Restaurant.name, content: Faker::Restaurant.description)
+end
+
+puts 'completed'
